8ef5948bfd1149111446e094d01c14c4021cf2af
Michaela Tremblay <michaelatrem08@gmail.com>
2015-12-09 16:21:49 -0500

    committing demo scripts

--- run.sh
+++ run.sh
@@ -4,12 +4,12 @@ string="ILDS"
 if [ "$1" == "ILDS" ]
 then
 	cd ../Solver/
-	./run.sh 10000 < ../FileFormats/complete-input-2 > ../Demo/ILDS.output
+	./run.sh 1200000 < ../FileFormats/add_times > ../Demo/ILDS.output
 	cd ../Validator
 	./run.sh ILDS
 else	
 	cd ../sLocal-Search/
-	node slocalsearch.js 10 ../Demo/Local.output <../FileFormats/complete-input-2
+	node slocalsearch.js 10 ../Demo/Local.output <../FileFormats/add_times
 	cd ../Validator
 	./run.sh Local
 fi
