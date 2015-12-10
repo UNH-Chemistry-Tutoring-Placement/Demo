aca87581326c786c1cf5b80e1cc3cc7756038e7c
Joel Barna <joel.tbarna@gmail.com>
2015-12-10 13:13:23 -0500

    Added correct node execution

--- run.sh
+++ run.sh
@@ -9,7 +9,7 @@ output=$3
 if [ "$alg" == "Local" ]
 then
     cd ../sLocal-Search/
-    node slocalsearch.js 10 "../Demo/$output" "../Demo/$input"
+    node slocalsearch.js 10 "../Demo/$output" < "../Demo/$input"
     cd ../Validator
     ./run.sh $alg $input $output
 else
