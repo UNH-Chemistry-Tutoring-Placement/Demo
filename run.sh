2c4d30b24b374a84d932e0c1198fce1c4c4c07cd
Stephen Chambers <stephenchambers515@gmail.com>
2015-12-10 16:09:34 -0500

    adding timeout as a command line option

--- run.sh
+++ run.sh
@@ -4,17 +4,17 @@ string="ILDS"
 alg=$1
 input=$2
 output=$3
-
+timeout=$4
 
 if [ "$alg" == "Local" ]
 then
     cd ../sLocal-Search/
-    node slocalsearch.js 10 "../Demo/$output" "../Demo/$input"
+    node slocalsearch.js $timeout "../Demo/$output" "../Demo/$input"
     cd ../Validator
     ./run.sh $alg $input $output
 else
     cd ../Solver/
-    ./run.sh 10000 < "../Demo/$input" > "../Demo/$output"
+    ./run.sh $timeout < "../Demo/$input" > "../Demo/$output"
     cd ../Validator
     ./run.sh $alg $input $output
 fi
--- solver.sh
+++ solver.sh
@@ -18,6 +18,11 @@ then
     echo "Did not enter an output file."
     exit
 fi
+if [ -z "$4" ]
+then
+    echo "Did not enter a timeout"
+    exit
+fi
 
 ./make.sh
-./run.sh $1 $2 $3
+./run.sh $1 $2 $3 $4
